1fa2b1a66afeea8bfd0d37ea013dc916bb5ba317
sayaliaher27 <68475674+sayaliaher27@users.noreply.github.com>
2021-08-29 14:37:42 +0530

    initial commit on math.py

---
+++ math.py
@@ -0,0 +1,16 @@
+# Add fn
+def add(x,y):
+    pass
+def sub(x,y):
+    return x-y      #on master branch
+def mul(x,y):
+    return x*y     # on bug456
+def div (x,y):
+    if(y==0):
+        return divideby0
+    else:
+        return x/y
+
+def square(x):
+    pass
+
